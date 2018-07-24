e9ab42759a884dbbdaa48d4521c18f58ed00a554
kanada-shou <41613041+kanada-shou@users.noreply.github.com>
2018-07-24 16:38:44 +0900

    Add files via upload

---
+++ hello.d
@@ -0,0 +1,21 @@
+// ----------------------------------------
+// @file - hello.d
+// @brief - 
+//
+// @note - 本書は 'xxmake' により作成された。
+//
+// @date - 2018/07/24 16:20:41
+//
+// [supp.] utf-8 : this file's encoding
+// ----------------------------------------
+
+import std.stdio;
+
+void main()
+{
+  writeln("Hello world!");
+}
+
+
+
+// End of File
